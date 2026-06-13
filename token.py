79c3d9f90ced92ae253b6515385109d72c948df4
Archit <architpanda453@gmail.com>
2026-06-14 00:09:31 +0530

    feat: implement token stream generation

--- token.py
+++ token.py
@@ -7,43 +7,97 @@ T_EOF = "EOF"
 
 
 class Token:
-  def __init__(self,type,value):
-    self.type = type
-    self.value = value
-  
-  def __repr__(self):
-    return f"Type = {self.type} ; Value = {self.value}"
-  
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def __repr__(self):
+        return f"Token({self.type}, {self.value})"
+
 
 class Lexer:
-  def __init__(self,source_code,pos = -1):
-    self.source_code = source_code
-    self.pos = pos
-    self.curr_char = ""
+    def __init__(self, source_code):
+        self.source_code = source_code
+        self.pos = 0
+        self.curr_char = source_code[0] if source_code else None
 
-  def advance(self):
-    self.pos += 1
-    self.curr_char = self.source_code[self.pos] if self.pos < len(self.source_code) else None
-  
-  def read_lexer(self):
-    word =""
-    while self.curr_char != " " and self.curr_char != None :
-      word += self.curr_char
-      self.advance()
-    return word
-  
-  def create_lexer_token(self):
-    word_created = self.read_lexer()
+    def advance(self):
+        self.pos += 1
 
-    if word_created == "let":
-      return Token(T_LET, word_created)
+        if self.pos < len(self.source_code):
+            self.curr_char = self.source_code[self.pos]
+        else:
+            self.curr_char = None
+
+    def skip_whitespace(self):
+        while self.curr_char is not None and self.curr_char.isspace():
+            self.advance()
+
+    def read_identifier(self):
+        word = ""
+
+        while (
+            self.curr_char is not None
+            and (self.curr_char.isalnum() or self.curr_char == "_")
+        ):
+            word += self.curr_char
+            self.advance()
+
+        return word
+
+    def read_number(self):
+        number = ""
+
+        while self.curr_char is not None and self.curr_char.isdigit():
+            number += self.curr_char
+            self.advance()
+
+        return number
+
+    def get_next_token(self):
+
+        while self.curr_char is not None:
+
+            if self.curr_char.isspace():
+                self.skip_whitespace()
+                continue
+
+            if self.curr_char.isalpha() or self.curr_char == "_":
+
+                word = self.read_identifier()
+
+                if word == "let":
+                    return Token(T_LET, word)
+
+                return Token(T_IDENTIFIER, word)
+
+            if self.curr_char.isdigit():
+                return Token(T_NUMBER, self.read_number())
+
+            if self.curr_char == "=":
+                self.advance()
+                return Token(T_ASSIGN, "=")
+
+            if self.curr_char == ";":
+                self.advance()
+                return Token(T_SEMICOLON, ";")
+
+            raise Exception(f"Unexpected character: {self.curr_char}")
+
+        return Token(T_EOF, None)
+    def tokenize(self):
+      tokens = []
+
+      while True:
+          token = self.get_next_token()
+          tokens.append(token)
+
+          if token.type == T_EOF:
+              break
+
+      return tokens
     
-    if type(int(word_created)) == int:
-      return TOKEN(T_NUMBER , word_created)
+lexer = Lexer("let num = 7;")
+tokens = lexer.tokenize()
 
-    return Token(T_IDENTIFIER, word_created)
-  
-lexer = Lexer("num")
-
-token = lexer.create_lexer_token()
-print(token)
+print(tokens)
